e996f162ed2670b2fb18301e2706339597fe5af0
vagrant <vagrant@homestead>
2021-04-29 09:29:24 +0000

    unread notification

---
+++ src/mixins/unreadCount.js
@@ -0,0 +1,31 @@
+import wepy from 'wepy'
+
+export default class unreadCount extends wepy.mixin {
+  data = {
+    intetval: null,
+    unreadCount: 0
+  }
+  onShow() {
+    this.updateUnreadCount()
+    this.intetval = setInterval(() => {
+      this.updateUnreadCount()
+    }, 30000);
+  }
+  onHide() {
+    clearInterval(this.intetval)
+  }
+  updateUnreadCount() {
+    this.unreadCount = this.$parent.globalData.unreadCount
+    this.$apply()
+    if (this.unreadCount) {
+      wepy.setTabBarBadge({
+        index: 1,
+        text: this.unreadCount.toString()
+      })
+    } else {
+      wepy.removeTabBarBadge({
+        index: 1
+      })
+    }
+  }
+}
